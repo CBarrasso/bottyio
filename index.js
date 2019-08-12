bd38853058fe366f1279b044de5a544c91a7bd7a
Chris <christopher.barrasso@gmail.com>
2019-08-12 17:33:47 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 var settings = require('./auth.json')
+var request = require("request");
+var parseString = require('xml2js').parseString;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -15,9 +17,47 @@ client.on('message', msg => {
 
 
 client.on('message', msg => {
-  if (msg.content === `what is ${user} dkp`) {
-    var text = fetch('https://www.mercenary-classic.com/dkp/api.php?function=points&filter=character&filterid='+user)
-    msg.reply(text)
+  if (msg.content.startsWith('dkp for')) {
+    var user = msg.content.split(" ").slice(-1)[0]
+    console.log(user)
+    var options = {
+      method: 'GET',
+      url: 'https://www.mercenary-classic.com/dkp/api.php?function=points',
+      headers:
+      {
+        Accept: '*/*'
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      
+      parseString(body, function (err, result) {
+
+        
+        
+        var users = result['response']['players'][0]['player']
+        found = false
+
+        console.log('USERS: '+JSON.stringify(users));
+        found = false
+        users.forEach(function(value){
+          // msg.reply(JSON.stringify(value));
+          if(value['name'][0]===user){
+            dkp = value['points'][0]['multidkp_points'][0]['points_current'][0]
+            msg.reply(value['name'][0]+ ' has ' + dkp + ' dkp.')
+            found = true
+          }
+
+
+
+        });
+        if(!found){
+          msg.reply(user + ' was not found.')
+        }
+      });
+
+    });
   }
 })
 
